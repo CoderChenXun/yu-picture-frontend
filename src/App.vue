18e9b63eccfd5f866cc1c0ca684ed41beb456e6b
coderLan <2463602852@qq.com>
2025-11-06 16:06:33 +0800

    init:init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <basic-layout></basic-layout>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BasicLayout from '@/layouts/BasicLayout.vue'
+import { healthUsingGet } from '@/api/mainController'
+import { useLoginUserStore } from '@/stores/user'
+
+healthUsingGet().then((response) => {
+  console.log(response)
+})
+
+const loginUserStore = useLoginUserStore()
+loginUserStore.fetchLoginUser()
+</script>
+
+<style scoped>
+#app {
+}
+</style>
